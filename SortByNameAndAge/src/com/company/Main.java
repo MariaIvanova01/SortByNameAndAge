7acc8ac40a72c443ac3b198b8cabc911b83ff8e9
MariaIvanova01 <mimi200801@abv.bg>
2022-07-08 16:21:55 +0300

    Using classes and comparator.

--- SortByNameAndAge/src/com/company/Main.java
+++ SortByNameAndAge/src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+
+        int countInouts = Integer.parseInt(scan.nextLine());
+
+        List<Person> people = new ArrayList<>();
+
+        for (int input = 0; input < countInouts; input++) {
+            String[] personInfo = scan.nextLine().split(" ");
+            String firstName = personInfo[0];
+            String lastName = personInfo[1];
+            int age = Integer.parseInt(personInfo[2]);
+            people.add(new Person(firstName,lastName,age));
+
+        }
+
+        Collections.sort(people, Comparator.comparing(Person::getFirstName).thenComparingInt(Person::getAge));
+
+        for (Person person : people) {
+            System.out.print(person.toString());
+        }
+
     }
 }
